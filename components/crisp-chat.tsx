8d64e6deb875d41ea32d035f053dda9b07567a2f
Krishi Shah <krishishah1211@gmail.com>
2025-10-05 15:14:06 -0700

    final

---
+++ components/crisp-chat.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Crisp } from "crisp-sdk-web";
+import { useEffect } from "react";
+
+export const CrispChat = () => {
+	useEffect(() => {
+		if (process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID) {
+			Crisp.configure(process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID);
+		} else {
+			console.error("CRISP WEBSITE ID is not defined.");
+		}
+	}, []);
+
+	return null;
+};
